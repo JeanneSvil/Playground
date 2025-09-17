f2d771879ca0f05e1f58d62b219ea99a968185ca
JeanneSvil <cerqliza@gmail.com>
2025-09-17 10:29:24 -0300

    atualizacao de arquivos v1.3

---
+++ Backend/Python/Analise de Dados/Cotacao/cotacao.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+from datetime import datetime
+import time
+
+for i in range(10):
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
+    cotacao_euro = requisicao_dic["EURBRL"]["bid"]
+    cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
+
+    tabela = pd.read_excel("Backend/Python/Analise de Dados/Cotacao/Cotações.xlsx")
+    tabela.loc[0, "Cotação"] = float(cotacao_dolar)
+    tabela.loc[1, "Cotação"] = float(cotacao_euro)
+    tabela.loc[2, "Cotação"] = float(cotacao_btc) * 1000
+    tabela.loc[0, "Data da Última Atualização"] = datetime.now()
+
+    tabela.to_excel("Cotações.xlsx", index=False)
+    print(f"Cotação Atualizada. {datetime.now()}")
+
+    time.sleep(30)
--- Backend/Python/Analise de Dados/Graficos com Plotly/grafico_gantt.py
+++ Backend/Python/Analise de Dados/Graficos com Plotly/grafico_gantt.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 
-tarefas = pd.read_excel('Backend/Python/Analise de Dados/2 - Gráficos com Python - Plotly/Tarefas.xlsx')
+tarefas = pd.read_excel('Backend/Python/Analise de Dados/Gráficos com Plotly/Tarefas.xlsx')
 fig = px.timeline(tarefas, x_start= "Início", x_end= "Fim", y= "Tarefa")
 fig.update_yaxes(autorange= 'reversed')
 fig.show(renderer= "browser")
